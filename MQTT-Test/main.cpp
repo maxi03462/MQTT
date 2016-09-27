d73ea0305dd27a16235189f6d2238b65cb97471d
electro2 <electro2@Debian>
2016-09-27 13:51:44 -0300

    Ahora tiene un log y el topic es "Test". *Mover el log a un archivo.

--- MQTT-Test/main.cpp
+++ MQTT-Test/main.cpp
@@ -1,23 +1,80 @@
 #include <QCoreApplication>
 #include <mosquitto.h>
+#include <stdio.h>
+
+#define printf qInfo
+
+
+void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
+{
+	if(message->payloadlen){
+		printf("%s %s\n", message->topic, message->payload);
+	}else{
+		printf("%s (null)\n", message->topic);
+	}
+	fflush(stdout);
+}
+
+void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
+{
+	int i;
+	if(!result){
+		/* Subscribe to broker information topics on successful connect. */
+		mosquitto_subscribe(mosq, NULL, "Test", 2);
+	}else{
+		printf("Connect failed\n");
+	}
+}
+
+void my_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos)
+{
+	int i;
+
+	printf("Subscribed (mid: %d): %d", mid, granted_qos[0]);
+	for(i=1; i<qos_count; i++){
+		printf(", %d", granted_qos[i]);
+	}
+	printf("\n");
+}
+
+void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str)
+{
+	/* Pring all log messages regardless of level. */
+	printf("%s\n", str);
+}
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+	QCoreApplication a(argc, argv);
+	
+	int i;
+	char *host = "localhost";
+	int port = 1883;
+	int keepalive = 60;
+	bool clean_session = true;
+	struct mosquitto *mosq = NULL;
 
-    struct mosquitto *mosq;
-    char clientid[24];
+	mosquitto_lib_init();
+	mosq = mosquitto_new(NULL, clean_session, NULL);
+	if(!mosq){
+		printf("Error: Out of memory.\n");
+		return 1;
+	}
+	mosquitto_log_callback_set(mosq, my_log_callback);
+	mosquitto_connect_callback_set(mosq, my_connect_callback);
+	mosquitto_message_callback_set(mosq, my_message_callback);
+	mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
 
-    qInfo("Listo para crear");
+	if(mosquitto_connect(mosq, host, port, keepalive)){
+		printf("Unable to connect.\n");
+		return 1;
+	}
 
-    mosq = mosquitto_new(clientid, true, NULL);
-    mosquitto_connect(mosq, "localhost", 1883,1500);
-    mosquitto_publish(mosq,NULL,"Test",10,"Hola Mundo", 0, true);
+	mosquitto_loop_forever(mosq, -1, 1);
 
-    qInfo("Enviado");
+    printf("Matar al mosquito.");
 
-    a.closingDown();
-
-    return a.exec();
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+	return a.exec();
 }
-
